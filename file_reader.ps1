c7e5783dc5dc9565973429b9181e25c7588d088e
Zachary Laney <theeliteoneshot@gmail.com>
2022-04-04 14:20:38 -0500

    Initial

---
+++ file_reader.ps1
@@ -0,0 +1,12 @@
+# Change Me
+###################################
+$InputFile = "./bytes.txt"
+$OutputFile = "./file_result.txt"
+###################################
+
+$SourceLines = [System.IO.File]::ReadAllLines($InputFile)
+$ByteArray = New-Object Byte[] $SourceLines.Length
+for($i=0;$i -lt $SourceLines.Length;$i++) {
+    $ByteArray[$i] = [int]$SourceLines[$i]
+}
+[System.IO.File]::WriteAllBytes('.text_result.txt' , $ByteArray)
